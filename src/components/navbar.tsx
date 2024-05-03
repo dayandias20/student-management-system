882c47ed18f21bc91f83d73a05991565235ddcbd
dayandias20 <dayandias20@gmail.com>
2024-05-02 20:21:10 -0400

    created routes and pages

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,7 +1,34 @@
+import { Link } from "react-router-dom";
+
 export const Navbar = () => {
     return(
         <div>
-            <h1>This is the NavBar</h1>
+            <center>
+                <nav className="navbar navbar-expand-lg bg-body-secondary">
+                    <div className="container-fluid">
+                        <a className="navbar navbar-brand" href="/">Navbar</a>
+                        <div className=" navbar-collapse" id="navbarNav">
+                            <ul className="navbar-nav">
+                                <li className="nav-item">
+                                    <Link className="nav-link" to="/home"> Home </Link>
+                                </li>
+                                <li className="nav-item">
+                                    <Link className="nav-link" to="/profile"> Profile </Link>
+                                </li>
+                                <li className="nav-item">
+                                    <Link className="nav-link" to="/register"> Register </Link>
+                                </li>
+                                <li className="nav-item">
+                                    <Link className="nav-link" to="/home"> Sign In </Link>
+                                </li>
+                                <li className="nav-item">
+                                    <Link className="nav-link" to="/home"> Sign Out </Link>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </nav>
+            </center>
         </div>
     );
 }
--- src/pages/register.tsx
+++ src/pages/register.tsx
@@ -1,123 +1,228 @@
-import { useState, useEffect, useRef } from "react";
-import { faCheck, faTimes, faInfoCircle } from "@fortawesome/free-solid-svg-icons";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import axios from 'axios';
+import React, { FormEvent, useEffect, useState } from 'react';
+// import { Student } from '../interfaces/Student';
 
-export const Register = () => {
+interface Student {
+    first_name: string;
+    last_name: string;
+    email: string;
+    phone: string;
+    address_line_1: string;
+    address_line_2: string;
+    address_city: string;
+    address_state: string;
+    address_zip: string;
+    password: string;
+    username: string;
+}
 
-    const USER_REGEX = /^[a-zA-Z][a-zA-Z0-9-_]{3,23}$/;
-    const PWD_REGEX = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@$%]).{8,32}$/;
+export const Register: React.FC = () => {
+    const [formData, setFormData] = useState<Student>({
+        first_name: '',
+        last_name: '',
+        email: '',
+        phone: '',
+        address_line_1: '',
+        address_line_2: '',
+        address_city: '',
+        address_state: '',
+        address_zip: '',
+        password: '',
+        username: ''
+    });
 
-    const userRef = useRef<HTMLInputElement>(null);
-    const errRef = useRef<HTMLDivElement>(null); 
-
-    const [ user, setUser ] = useState<string>('');
-    const [ validName, setValidName ] = useState<boolean>(false);
-    const [ userFocus, setUserFocus ] = useState<boolean>(false);
-
-    const [ pwd, setPwd ] = useState<string>('');
-    const [ validPwd, setValidPwd ] = useState<boolean>(false);
-    const [ pwdFocus, setPwdFocus ] = useState<boolean>(false);
-
-    const [ matchPwd, setMatchPwd ] = useState<string>('');
-    const [ validMatch, setValidMatch ] = useState<boolean>(false);
-    const [ matchFocus, setMatchFocus ] = useState<boolean>(false);
-    
-    const [ errMsg, setErrMsg ] = useState<string>('');
-    const [ success, setSuccess ] = useState<boolean>(false)
-
-    const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
-        e.preventDefault();
-
-        // If the button was hacked and trying to pass invalid data
-        const v1 = USER_REGEX.test(user);
-        const v2 = PWD_REGEX.test(pwd);
-
-        if(!v1 || !v2) {
-            setErrMsg("invalid entry");
-            return;
-        }
-
-        console.log(user, pwd)
-        setSuccess(true);
+    const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        const { name, value } = e.target;
+        setFormData(prevState => ({
+            ...prevState,
+            [name]: value
+        }));
+        // console.log(formData);
     };
 
-    useEffect( () => {
-        userRef.current?.focus();
-    }, []) 
+    const saveStudentData = async (event: FormEvent<HTMLFormElement>): Promise<void> => {
+        event.preventDefault();
 
-    // to validate the User
+        try{
+            const response = await axios.post("http://localhost:8080/new_student/save", formData)
+            console.log(response.data);
+            setFormData({
+                first_name: '',
+                last_name: '',
+                email: '',
+                phone: '',
+                address_line_1: '',
+                address_line_2: '',
+                address_city: '',
+                address_state: '',
+                address_zip: '',
+                password: '',
+                username: ''
+            });
+        }
+        catch(error) {
+            console.log("Error saving student");
+        }
+    }
+
     useEffect(() => {
-        USER_REGEX.test(user) ? setValidName(USER_REGEX.test(user)) : console.log("User name : ", user, " not valid");
-    }, [user]);
+        
+    },[])
+    return (
 
-    useEffect(() => {
-        const result = PWD_REGEX.test(pwd);
-        setValidPwd(result);
-        const match = pwd === matchPwd;
-        setValidMatch(match);
-    }, [ pwd, matchPwd ]);
+        <div className='container'>
+            <br />
+            <br />
+            <center>
+                <form onSubmit={saveStudentData}>
 
-    useEffect(() => {
-        setErrMsg('')
-    }, [user, pwd, matchPwd])
+                
+            <div className='row row-cols-1 row-cols-md-2 row-cols-lg-2 g-4'>
+                <div >   
+                    <input
+                    type="text"
+                    name="first_name"
+                    value={formData.first_name}
+                    onChange={handleChange}
+                    placeholder="First Name"
+                    />
+                </div>
+                <div >
+                    <input
+                    type="text"
+                    name="last_name"
+                    value={formData.last_name}
+                    onChange={handleChange}
+                    placeholder="Last Name"
+                    />
+                </div>
+                <div >   
+                    <input
+                    type="text"
+                    name="email"
+                    value={formData.email}
+                    onChange={handleChange}
+                    placeholder="Email"
+                    />
+                </div>
+                <div>
+                    <input
+                    type="text"
+                    name="phone"
+                    value={formData.phone}
+                    onChange={handleChange}
+                    placeholder="Phone"
+                    />
+                </div>
+            </div>
 
-    return(
-        <section>
-            <p ref={errRef} className={errMsg ? "errmsg" : "offscreen"} aria-live="assertive" > { errMsg } </p>
-            <h1> Register </h1>
+            <br />
+            <div className='container'>
+                <h3>
+                    Address Information
+                </h3>
+                <br />
+                <div className='row row-cols-1 row-cols-md-2 row-cols-lg-3 g-4'>
+                    <div>   
+                        <input
+                        type="text"
+                        name="address_line_1"
+                        value={formData.address_line_1}
+                        onChange={handleChange}
+                        placeholder="Street Address"
+                        />
+                    </div>
+                    <div>
+                        <input
+                        type="text"
+                        name="address_line_2"
+                        value={formData.address_line_2}
+                        onChange={handleChange}
+                        placeholder="Apt/Floor/Room"
+                        />
+                    </div>
+                    <div>   
+                        <input
+                        type="text"
+                        name="address_city"
+                        value={formData.address_city}
+                        onChange={handleChange}
+                        placeholder="City"
+                        />
+                    </div>
+                    <div>
+                        <input
+                        type="text"
+                        name="address_state"
+                        value={formData.address_state}
+                        onChange={handleChange}
+                        placeholder="State"
+                        />
+                    </div>
+                    <div>
+                        <input
+                        type="text"
+                        name="address_zip"
+                        value={formData.address_zip}
+                        onChange={handleChange}
+                        placeholder="Zip Code"
+                        />
+                    </div>
 
-            <form onSubmit={handleSubmit}>
-                {/* username input */}
-                <label htmlFor="username"> 
-                    Username : 
-                    <FontAwesomeIcon icon={faCheck} className={validName ? "valid" : "hide"} />
-                    <FontAwesomeIcon icon={faTimes} className={validName || !user ? "hide" : "invalid"} />
-                </label>
-                <input id='username' type="text" required ref={userRef} autoComplete="off" onChange={(e) => setUser(e.target.value)} aria-invalid={validName ? "false" : "true"} aria-describedby="uidnote" onFocus={() => setUserFocus(true)} onBlur={()=>setUserFocus(false)}></input>
-                <p id="uidnote" className={userFocus && user && !validName ? "instructions" : "offscreen"}>
-                    <FontAwesomeIcon icon={faInfoCircle} />
-                    4 to 24 characters. <br/>
-                    Must begin with a letter. <br />
-                    Letters, numbers, underscores, hyphens allowed.
-                </p>
-
-                {/* pwd input */}
-                <label htmlFor="password">
-                    Password : 
-                    <FontAwesomeIcon icon={faCheck} className={validPwd ? "valid" : "hide"} />
-                    <FontAwesomeIcon icon={faTimes} className={validPwd || !pwd ? "hide" : "invalid"} />
-                </label>
-                <input id="password" type="password" required onChange={(e)=>setPwd(e.target.value)}  aria-invalid={validPwd ? "false":"true"} aria-describeby="pwdnote" onFocus={()=>setPwdFocus(true)} onBlur={()=>{setPwdFocus(false)}}></input>
-                <p id="pwdnote" className={pwdFocus && !validPwd ? "instructions" : "offscreen"}>
-                    <FontAwesomeIcon icon={faInfoCircle} /> 
-                    8 to 24 characters. <br />
-                    Must include uppercase and lowercase letters, a number and a special character. <br />
-                    Allowed special characters: <span aria-label="exclamation mark">!</span>
-                                                <span aria-label="at symbol">@</span> 
-                                                <span aria-label="hashtag">#</span>
-                                                <span aria-label="dollar sign">$</span>
-                                                <span aria-label="percent">%</span>
-                </p>
-                {/* pwd match */}
-                <label htmlFor="confirm_pwd">
-                    Confirm Password : 
-                    <FontAwesomeIcon icon={faCheck} className={validMatch && matchPwd ? "valid" : "hide"} />
-                    <FontAwesomeIcon icon={faTimes} className={validMatch || !matchPwd ? "hide" : "invalid"} />
-                </label>
-                <input id="confirm_pwd" type="password" required onChange={(e)=>{setMatchPwd(e.target.value)}} aria-invalid={validMatch ? "false" : "true" } aria-describedby="confirmnote" onFocus={()=>setMatchFocus(true)} onBlur={()=>setMatchFocus(false)}/>
-                <p id="confirmnote" className={matchFocus && !validMatch ? "instructions":"offscreen"}> 
-                    <FontAwesomeIcon icon={faInfoCircle} />
-                    Must match the first password input field
-                </p>
-                <button disabled={!validName || !validPwd || !validMatch ? true : false}> Sign Up </button>
+                </div>
+            </div>
+<br />
+            <div className='container'>
+                <h3>
+                    Username and Password
+                </h3>
+                <br />
+                <div className='row row-cols-1 row-cols-md-2 row-cols-lg-3 g-4'>
+                    <div>   
+                        <input
+                        type="text"
+                        name="username"
+                        value={formData.username}
+                        onChange={handleChange}
+                        placeholder="Username"
+                        />
+                    </div>
+                    <div>
+                        <input
+                        type="password"
+                        name="password"
+                        value={formData.password}
+                        onChange={handleChange}
+                        placeholder="Password"
+                        />
+                    </div>
+                    <div>
+                        <input
+                        type="password"
+                        name="confirm_password"
+                        value={formData.password}
+                        onChange={handleChange}
+                        placeholder="Confirm Password"
+                        />
+                    </div>
+                </div>
+            </div>
+            <br />
+            <div className='container'>
+                
+                <div className='row row-cols-1 row-cols-md-3 row-cols-lg-3 g-4'>
+                    <div></div>
+                    <div><button className='btn btn-secondary'>Submit</button></div>
+                    <div></div>
+                </div>
+            </div>
             </form>
-            <p>
-                Already registered? <br />
-                <span className="line">
-                    {/* put router link here */}
-                    <a href="#"> Sign in</a>
-                </span>
-            </p>
-        </section>
+            </center>
+            
+            
+                
+        </div>
     );
-}
+};
+
+
